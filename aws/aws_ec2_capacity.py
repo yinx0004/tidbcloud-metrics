c75c414d7841940211e8e509e333c333d671aee4
Sherry <yinx0004@gmail.com>
2024-07-11 11:32:52 +0800

    support c6g.16xlarge ec2 instance

--- aws/aws_ec2_capacity.py
+++ aws/aws_ec2_capacity.py
@@ -14,5 +14,6 @@ ec2_resource_capacity = {
     'm6g.2xlarge': {'CPU(core)': 8, 'Memory(byte)': 34359738368, 'NetworkIn Bandwidth(byte)': 335544320, 'NetworkOut Bandwidth(byte)': 335544320, 'Disk Bandwidth(byte)': 311301242.88, 'Disk IOPS': 4000},
     'c6g.xlarge': {'CPU(core)': 4, 'Memory(byte)': 8589934592, 'NetworkIn Bandwidth(byte)': 167772160, 'NetworkOut Bandwidth(byte)': 167772160, 'Disk Bandwidth(byte)': 131072000, 'Disk IOPS': 3000},
     'c6g.large': {'CPU(core)': 2, 'Memory(byte)': 4294967296, 'NetworkIn Bandwidth(byte)': 100663296, 'NetworkOut Bandwidth(byte)': 100663296, 'Disk Bandwidth(byte)': 82575360, 'Disk IOPS': 3600},
-    'm6g.16xlarge': {'CPU(core)': 64, 'Memory(byte)': 274877906944, 'NetworkIn Bandwidth(byte)': 26843545600, 'NetworkOut Bandwidth(byte)': 26843545600, 'Disk Bandwidth(byte)': 2490368000, 'Disk IOPS': 80000}
+    'm6g.16xlarge': {'CPU(core)': 64, 'Memory(byte)': 274877906944, 'NetworkIn Bandwidth(byte)': 26843545600, 'NetworkOut Bandwidth(byte)': 26843545600, 'Disk Bandwidth(byte)': 2490368000, 'Disk IOPS': 80000},
+    'c6g.16xlarge': {'CPU(core)': 64, 'Memory(byte)': 137438953472, 'NetworkIn Bandwidth(byte)': 26843545600, 'NetworkOut Bandwidth(byte)': 26843545600, 'Disk Bandwidth(byte)': 2490368000, 'Disk IOPS': 80000},
 }
